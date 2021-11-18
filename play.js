4639a04e8aed279326da6b71c9a4b531b134f0c4
Umar Khan <umarkhan167@outlook.com>
2021-11-17 23:32:58 -0500

    can exit snek call by running ctrl + c in node terminal

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
+const { stdin } = require("process");
 const clientConnect = require("./client");
 
 clientConnect;
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin.on("data", handleUserInput);
+
+setupInput;
